8c9a1ad9d8c211f4d96b0285e2f222ad6456c285
Fruscianty <anri.2002.xndr@gmail.com>
2024-11-25 17:02:10 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -70,4 +70,74 @@ for (let i=0; i<arr.length; i++) {
 }
 
 //ass4
+let arr = [1, 2, 3, 4, 5, 6];
+arr.splice(2, 2); 
+console.log (arr);
+
+arr.splice(1, 0, 7, 8)
+console.log (arr);
+
+let lastThree = arr.slice(-3); 
+console.log(arr); 
+console.log(lastThree);
+
+//ass5
+let arr = ["Anri", "Jaba", "Saba", "Tornike", "Giorgi"];
+arr.sort(); 
+console.log (arr);
+
+// arr.reverse();
+// console.log (arr);
+
+arr.sort((a, b) => b.localeCompare(a));
+console.log(arr);
+
+//ass6
+let arr = [1, 4, 12, 17, 20];
+console.log(arr.indexOf(12));
+
+console.log(arr.includes(12));
+
+console.log(arr.findIndex(num => num > 5))
+
+let indexForLoop = -1;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 12) {
+    indexForLoop = i;
+    break;  
+  }
+}
+
+console.log (indexForLoop);
+
+//ass10
+let arr = [5, 2, 8, 1, 4];
+arr.reverse();
+console.log(arr); 
+
+arr.sort((a, b) => b - a);
+console.log(arr);
+
+//ass8
+let arr = ["Strawberry", "Juice", "Soda", "Bottle", "Taste"];
+
+let [first, second] = arr;
+console.log (first);
+console.log(second);
+
+[second, first] = [first, second];
+console.log (first);
+console.log(second);
+
+//ass9
+let arr = [9, 7, 22, 23];
+let num = [12, 17, 0, 11];
+
+let newArr = arr.concat(num);
+console.log(newArr)
+
+let arrNew = [...arr, ...num];
+console.log (arrNew);
+
+//ass7
 
